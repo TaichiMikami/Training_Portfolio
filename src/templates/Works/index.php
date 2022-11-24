d15bc8b30432881202fbfbdd362793c50957818d
TaichiMikami <tasmjtaichi@gmail.com>
2022-11-24 19:02:13 +0900

    2022_1124_1900

--- src/templates/Works/index.php
+++ src/templates/Works/index.php
@@ -5,30 +5,43 @@
  */
 ?>
 
-<header>
-    <div class = "title">Taichi's Portfolio</div>
-</header>
-<div class = main>
-    <h1>recently works</h1>
-    <?php foreach ($works as $work): ?>
-        <?php if($work->display == 2){continue;} ?>
-        <div class = "song_container">
-            <ul>
-                <li class = movie>
-                <iframe width="320" height="180" src="https://www.youtube.com/embed/{$this->link}
-                    ?loop=1 &mute=1 &modestbranding=1"
-                    frameborder="0" allowfullscreen>
-                </iframe>
-                </li>
-                <li class = title>
-                    <?= h($work->artist) ?> - <?= h($work->title) ?>
-                </li>
-                <li class = type>
-                    <?= $work->getTypeText() ?>
-                    <?= $work->getMasteringText() ?>
-                </li>
+<div class="header">
+    <div class="header-wrapper">
+        <a href="#" class="site-title">taichi's portfolio</a>
+        <nav class="nav">
+            <ul class="nav-wrapper">
+                <li class="nav-item"><a href="#">home</a></li>
+                <li class="nav-item"><a href="#">price</a></li>
+                <li class="nav-item"><a href="#">contact</a></li>
             </ul>
-        </div>
-    <?php endforeach; ?>
+        </nav>
+    </div>
 </div>
+<div class = "main">
+    <div class = "main-wrapper">
+        <h1>recently works</h1>
+        <?php foreach ($works as $k => $work): ?>
+            <?php if($work->display == 2){continue;} ?>
+            <?php $position = $k % 2 + 1 ?>
+            <div class = "song-container-<?php echo $position ?>">
+                <div class = "movie">
+                    <iframe width="320" height="180" 
+                        src="https://www.youtube.com/embed/<?= $work->link ?>?loop=1&mute=1&modestbranding=1"
+                        frameborder="0" allowfullscreen>
+                    </iframe>
+                </div>
+                <div class = "text-container">
+                    <p class = "text">
+                        <?= h($work->artist) ?> - <?= h($work->title) ?>
+                        <br>
+                        <?= $work->getTypeText() ?>
+                        <?= $work->getMasteringText() ?>
+                    </p>
+                </div>
+            </div>
+        <?php endforeach; ?>
+    </div>
+</div>
+<div class = footer>
+
 </div>
